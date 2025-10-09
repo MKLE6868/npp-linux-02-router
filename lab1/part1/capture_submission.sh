ada1519f2a278171bb5ecbeb365fb18c92a8a736
MKLE6868 <56886214+MKLE6868@users.noreply.github.com>
2025-10-09 00:13:13 -0400

    Update capture_submission.sh

--- lab1/part1/capture_submission.sh
+++ lab1/part1/capture_submission.sh
@@ -10,7 +10,7 @@ rtrD="docker exec -it  clab-mod2-bgp-rtrD"
 filename="submission.out"
 
 part1 () {
-$lan1 traceroute -s 1.1.1.1 4.4.4.4 > $filename
+$lan1 traceroute -n -s 1.1.1.1 4.4.4.4 > $filename
 echo "---" >> $filename
 $rtrB birdc show route all >> $filename
 echo "---" >> $filename
@@ -19,7 +19,7 @@ echo "---" >> $filename
 }
 
 part2() {
-$lan1 traceroute -s 1.1.1.1 4.4.4.4 >> $filename
+$lan1 traceroute -n -s 1.1.1.1 4.4.4.4 >> $filename
 }
 
 if [ $# != 1 ]
